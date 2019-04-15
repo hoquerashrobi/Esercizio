ea25bf92184d8e127034de8a28cf9b5005022621
Hoque <hoque.rashedul@iisgalvanimi.edu.it>
2019-04-15 14:02:33 +0000

    punto 1

--- calcolatrice/src/app/app.component.ts
+++ calcolatrice/src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calcolatrice';
+
+ r: number;
+    num_vett : number[];
+    somma(n1: HTMLInputElement, n2: HTMLInputElement) :Boolean {
+        this.r = Number(n1.value) + Number(n2.value);
+
+        this.fillVect(Number(n1.value), Number(n2.value));
+        return false;
+    }
+
+    fillVect(n1: number, n2:number)
+    {
+        //Se n2< n1 li scambio
+        if (n1<n2) {
+            let a = n2;
+            n2 = n1;
+            n2= a;
+        }
+        this.num_vett = new Array<number>();
+        for(let i = n1; i <=n2; i++)
+        {
+            console.log(i);
+            this.num_vett.push(i);
+        }
+    }
+
+
+
 }
